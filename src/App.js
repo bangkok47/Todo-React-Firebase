6898cbf8fba484fc08aff2964e6836fe5eda92b8
bangkok47 <bangkok2422@gmail.com>
2021-03-22 19:05:12 +0200

    add button and todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core';
+
+
+
 
 function App() {
+  const [todos, setTodos] = useState(['Task 1', 'Task 2', 'Task 3']);
+  const [input, setInput] = useState('');
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO APP</h1> 
+        <form>
+         <FormControl>
+           <InputLabel>Write a todo</InputLabel>
+           <Input value={input} onChange={e => setInput(e.target.value)}/>
+         </FormControl>
+         
+          <Button disabled={!input} variant="contained" type='submit' onClick={addTodo} color="primary">Add Todo</Button>
+        </form>
+     
+     <ul>
+     {todos.map(todo => (
+       <li>{todo}</li>
+     ))}
+     </ul>
     </div>
   );
 }
